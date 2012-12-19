9312db8e1c973374e180653ba248e77438049a75
Jesper Brown <jesper.brown@gmail.com>
2012-12-19 11:45:25 +0100

    fixed bug in installer

--- gitcompletion/install.sh
+++ gitcompletion/install.sh
@@ -2,10 +2,10 @@
 # Will setup git completion in your home directory
 if [[ -f "gitcompletion.tar" ]]; then
   tar -xvf gitcompletion.tar -C ~/
-  if [[ -f "~/.bash_profile" ]]; then
-    bash_file="~/.bash_profile"
+  if [[ -f ~/.bash_profile ]]; then
+    bash_file=~/.bash_profile
   else 
-    bash_file="~/.bashrc"
+    bash_file=~/.bashrc
   fi
   echo "source ~/.gitcompletion/git-completion.bash" >> $bash_file
 fi
